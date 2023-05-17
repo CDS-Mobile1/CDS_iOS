293ce5f9e4512398a2a72969c87092841bc9f973
seongmin221 <seongmin221@naver.com>
2023-05-17 20:55:38 +0900

    [Chore] #6 - added BaseTableViewCell

--- Instagram-iOS/Instagram-iOS/Global/Base/BaseTableViewCell.swift
+++ Instagram-iOS/Instagram-iOS/Global/Base/BaseTableViewCell.swift
@@ -5,4 +5,38 @@
 //  Created by 이성민 on 2023/05/17.
 //
 
-import Foundation
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    // MARK: - Property
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setLayout()
+        setStyle()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting
+    
+    func setLayout() {
+        // override to use
+    }
+    
+    func setStyle() {
+        // override to use
+    }
+    
+}
