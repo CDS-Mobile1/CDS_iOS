fc00e23303468782340780a8f414f73008c43326
seongmin221 <seongmin221@naver.com>
2023-05-19 00:19:08 +0900

    [Feat] #18 - UserProfileView added

---
+++ Instagram-iOS/Instagram-iOS/Global/UIComponent/UserProfileView.swift
@@ -0,0 +1,41 @@
+//
+//  UserProfileView.swift
+//  Instagram-iOS
+//
+//  Created by 이성민 on 2023/05/19.
+//
+
+import UIKit
+
+import SnapKit
+
+final class UserProfileView: UIView {
+    
+    // MARK: - Property
+    
+    let profileImageView: UIImageView = {
+//        let imageView = UIImageView(image: )
+    }()
+    
+    // MARK: - UI Property
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    
+    
+    // MARK: - Setting
+    
+    
+    
+    // MARK: - Action Helper
+    
+    
+    
+    // MARK: - Custom Method
+    
+    
+    
+    
+}
