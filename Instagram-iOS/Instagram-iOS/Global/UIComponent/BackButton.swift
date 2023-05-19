754e05670aebd93084b1d292c651a98b55a34978
seongmin221 <seongmin221@naver.com>
2023-05-19 21:31:17 +0900

    [Chore] #15 - add back uicomponent

---
+++ Instagram-iOS/Instagram-iOS/Global/UIComponent/BackButton.swift
@@ -0,0 +1,20 @@
+//
+//  BackButton.swift
+//  Instagram-iOS
+//
+//  Created by 이성민 on 2023/05/19.
+//
+
+import UIKit
+
+final class BackButton: UIButton {
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setImage(ImageLiteral.NavBar.back, for: .normal)
+    }
+        
+}
