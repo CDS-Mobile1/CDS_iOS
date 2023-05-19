4a81ef053be37494982361bb7ceb4242f8fef585
seongmin221 <seongmin221@naver.com>
2023-05-19 16:34:48 +0900

    [Chore] #15 - postcontentTextView added

--- Instagram-iOS/Instagram-iOS/Screens/NewPost/NewPostViewController.swift
+++ Instagram-iOS/Instagram-iOS/Screens/NewPost/NewPostViewController.swift
@@ -17,23 +17,48 @@ final class NewPostViewController: BaseViewController {
     
     // MARK: - UI Property
     
-    let dummyView = UIView()
+    // TODO: collectionview 추가
+    let dummyView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green1
+        return view
+    }()
     
     private lazy var postContentTextView: UITextView = {
         let textView = UITextView()
         textView.text = postContentPlaceholder
-        
+        textView.textColor = .gray2
+        textView.font = .bodyKor
         return textView
     }()
     
     // MARK: - Life Cycle
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hideKeyboardWhenTappedAround()
+    }
     
     // MARK: - Setting
     
     override func setDelegate() {
         postContentTextView.delegate = self
+    }
+    
+    override func setLayout() {
+        view.addSubview(dummyView)
+        dummyView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(80)
+        }
+        
+        // TODO: 영역 정해지면 inset 값 수정하기
+        view.addSubview(postContentTextView)
+        postContentTextView.snp.makeConstraints {
+            $0.top.equalTo(dummyView.snp.bottom)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
     }
     
     // MARK: - Action Helper
@@ -55,7 +80,14 @@ extension NewPostViewController: UITextViewDelegate {
     func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.text == postContentPlaceholder {
             textView.text = nil
-//            textView.textColor = UIColor.black1
+            textView.textColor = .black1
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = postContentPlaceholder
+            textView.textColor = .gray2
         }
     }
     
