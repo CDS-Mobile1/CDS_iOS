1974260e382c86345abb02f2815377bb459d6205
Joon Baek <joonbaek12@gmail.com>
2023-06-03 01:29:18 +0900

    [Chore] #27 - DMTableViewCellHeader에 마크주석 추가

--- Instagram-iOS/Instagram-iOS/Screens/DMList/Cell/DMTableViewCellHeader.swift
+++ Instagram-iOS/Instagram-iOS/Screens/DMList/Cell/DMTableViewCellHeader.swift
@@ -10,8 +10,11 @@ import UIKit
 import SnapKit
 
 final class DMTableViewCellHeader: UITableViewHeaderFooterView {
-
+    // MARK: - Property
+    
     static let identifier = "DMTableViewCellHeader"
+
+    // MARK: - UI Property
     
     private let sectionLabel: UILabel = {
         let label = UILabel()
@@ -20,6 +23,8 @@ final class DMTableViewCellHeader: UITableViewHeaderFooterView {
         label.text = "메시지"
         return label
     }()
+    
+    // MARK: - Life Cycle
     
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
@@ -31,10 +36,11 @@ final class DMTableViewCellHeader: UITableViewHeaderFooterView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Layout
+    
     private func setLayout() {
         
         addSubview(sectionLabel)
-        
         sectionLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(16)
             $0.bottom.equalToSuperview().offset(-8)
